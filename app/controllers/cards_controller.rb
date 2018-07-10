b28efca8c377f53b1d47f8d1e4888032e96e6e13
Cernan Bernardo <cernanb@gmail.com>
2018-07-10 16:30:32 -0700

    Start actions in cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,30 @@
 class CardsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def index
+    @cards = Card.all
+  end
+
+  def show
+
+  end
 end
