e6de7214496ad183aa14023718e51a4056971777
Cernan Bernardo <cernanb@gmail.com>
2018-07-10 16:50:43 -0700

    Start UserCards controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  
   def new
 
   end
@@ -23,6 +25,11 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    
   end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
