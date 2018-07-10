25a8eae2e853bc8086b52ea9d0ec3ff2889ff0b7
Cernan Bernardo <cernanb@gmail.com>
2018-07-10 16:50:31 -0700

    Add form to create a new card & build create action in cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,17 @@
 class CardsController < ApplicationController
 
   def new
-
+    @card = Card.new
   end
 
   def create
+    @card = Card.new(card_params)
 
+    if @card.save
+      redirect_to cards_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -27,4 +33,10 @@ class CardsController < ApplicationController
   def show
 
   end
+
+  private
+
+    def card_params
+      params.require(:card).permit(:player_name, :player_team)
+    end
 end
