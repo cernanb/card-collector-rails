16314fcbd2bc7755ec83b189270e4af1bd0edf01
Cernan Bernardo <cernanb@gmail.com>
2018-07-11 08:09:28 -0700

    Draw callback route for facebook oauth

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+
+  get '/auth/facebook/callback', to: 'sessions#create'
   root "application#home"
+
+
 end
