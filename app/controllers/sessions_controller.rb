ab367be3a7d29d6d61ece97ad5a85091d1cfb48e
Cernan Bernardo <cernanb@gmail.com>
2018-07-11 08:08:35 -0700

    Handle facebook oauth in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:user][:email])
+    if request.env['omniauth.auth']
+      @user = User.find_by(email: request.env['omniauth.auth']["info"]["email"])
 
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      if @user 
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      end
     else
-      render :new
+      @user = User.find_by(email: params[:user][:email])
+
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        render :new
+      end
     end
 
   end
